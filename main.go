d8b7bd7973d2437625a39e1a0a0950caa0cc8d73
Artem Innokentiev <artinnok@mail.ru>
2018-12-20 23:50:26 +0900

    добавил базовую структуру LongURL и пару эндпоинтов - /hello/:username, /shortify

--- main.go
+++ main.go
@@ -2,18 +2,36 @@ package main
 
 import (
 	"net/http"
-
+	"fmt"
+	
 	"github.com/labstack/echo"
 )
+
+type LongURL struct {
+	URL string `json:"url"`
+}
+
+func hello(c echo.Context) error {
+	username := c.Param("username")
+	res := fmt.Sprintf("Hello, %s!", username)
+
+	return c.JSON(http.StatusOK, res)
+}
+
+func shortify(c echo.Context) (err error) {
+	url := &LongURL{}
+	if err = c.Bind(url); err != nil {
+		return echo.NewHTTPError(http.StatusForbidden, "Invalid data")
+	}
+
+	return c.JSON(http.StatusOK, url)
+}
 
 func main() {
 	e := echo.New()
 
-	e.GET("/", hello)
+	e.GET("/hello/:username", hello)
+	e.POST("/shortify", shortify)
 
 	e.Logger.Fatal(e.Start(":9000"))
-}
-
-func hello(c echo.Context) error {
-	return c.String(http.StatusOK, "Hello, World!")
 }
